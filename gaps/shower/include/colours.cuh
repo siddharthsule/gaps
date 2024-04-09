dccc1cda46d206e5f6f27db938b830ab97bf86f9
Siddharth Sule <siddharthsule@outlook.com>
2024-04-09 21:00:50 +0100

    return to og sol

--- gaps/shower/include/colours.cuh
+++ gaps/shower/include/colours.cuh
@@ -3,4 +3,9 @@
 
 #include "qcd.cuh"
 
+// Colours
+__device__ void MakeColours(Event &ev, int *coli, int *colj, const int flavs[3],
+                            const int colij[2], const int colk[2],
+                            const int rand);
+
 #endif // SHOWER_COLOURS_CUH_
