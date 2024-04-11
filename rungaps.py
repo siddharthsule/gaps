2b4ee59d4cf76fe6997d1312b7439d9e62de859d
Siddharth Sule <siddharthsule@outlook.com>
2024-04-11 18:32:37 +0100

    bugfix typo and updates

--- rungaps.py
+++ rungaps.py
@@ -19,16 +19,25 @@ import os
 import shutil
 import subprocess
 
+# ------------------------------------------------------------------------------
+
 # Set up argument parser
 parser = argparse.ArgumentParser(description='Run GAPS or C++ Shower')
-parser.add_argument('-n', type=int, default=10000, help='set the number of events (default: 10000)')
-parser.add_argument('-e', type=float, default=91.2, help='set the CoM energy of the system (default: 91.2)')
-parser.add_argument('-c', type=int, default=1, help='set the number of cores (default: 1)')
-parser.add_argument('-r', type=str, default='gaps', help='set the run type (default: gaps, options: gaps, cpp, compare, full)')
+parser.add_argument('-n', '--nevents', type=int, default=10000,
+                    help='set the number of events (default: 10000)')
+parser.add_argument('-e', '--energy', type=float, default=91.2,
+                    help='set the CoM energy of the system (default: 91.2)')
+parser.add_argument('-c', '--cores', type=int, default=1,
+                    help='set the number of cores (default: 1)')
+parser.add_argument('-r', '--runtype', type=str, default='gaps',
+                    help='set the run type (default: gaps, options: gaps, cpp, compare, full)')
 
 args = parser.parse_args()
 
+# ------------------------------------------------------------------------------
 # Compile code
+
+
 def compile(dir):
     print(f'Compiling {dir}')
     os.chdir(dir)
@@ -36,13 +45,20 @@ def compile(dir):
     os.chdir('build')
     subprocess.run(['cmake', '..'])
     subprocess.run(['make', '-j', str(args.c)])
+    os.chdir('../..')
 
+# ------------------------------------------------------------------------------
 # Run GAPS or C++ Shower
+
+
 def run(runtype, events, energy):
     print(f'Running {runtype}')
     subprocess.run([f'./{runtype}/bin/{runtype}', str(events), str(energy)])
 
+
+# ------------------------------------------------------------------------------
 # Compile and run based on runtype
+
 if args.r in ['gaps', 'compare']:
     compile('gaps')
     run('gaps', args.n, args.e)
@@ -50,7 +66,6 @@ if args.r in ['gaps', 'compare']:
 if args.r in ['cpp', 'compare']:
     compile('cpp-shower')
     run('cpp-shower', args.n, args.e)
-
 
 if args.r == 'full':
     # Remove previous results and make folder
@@ -65,17 +80,21 @@ if args.r == 'full':
         os.remove('gaps-time.dat')
 
     # Run the comparison 100 times, for different number of events
-    neventsarray = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
+    neventsarray = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000,
+                    5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
     for n in neventsarray:
         # Run and store the output in a log file
         for i in range(1, 101):
             print(f"Running GAPS with {n} events")
             subprocess.run(['./gaps/bin/gaps', str(n), str(args.e)])
             print(f"Running C++ Shower with {n} events")
-            subprocess.run(['./cpp-shower/bin/cpp-shower', str(n), str(args.e)])
+            subprocess.run(
+                ['./cpp-shower/bin/cpp-shower', str(n), str(args.e)])
 
     # Move the log files to the results directory
     shutil.move('cpp-time.dat', 'results/')
     shutil.move('gaps-time.dat', 'results/')
     shutil.move('cpp.yoda', 'results/')
     shutil.move('gaps.yoda', 'results/')
+
+# ------------------------------------------------------------------------------
