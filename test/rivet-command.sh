59b33c7f2b5d0d99c5329066c826401705c2f39e
Siddharth Sule <siddharthsule@outlook.com>
2024-04-24 07:53:43 +0100

    update rivet command and add paper results as a zip

--- test/rivet-command.sh
+++ test/rivet-command.sh
@@ -1,6 +1,6 @@
 # Command to Make Plots of Jet and Event Shapes
 # Write something similar in the terminal
 rivet-mkhtml -s --mc-errs -c plots.conf \
-    ../cpp.yoda:"C++" \
-    ../gaps.yoda:"GAPS" \
-    SH-Tutorial.yoda:"S. H." \
+    ../results-times/cpp.yoda:"C++" \  
+    ../results-times/gaps.yoda:"CUDA" \
+    SH-Tutorial.yoda:"S. H."
