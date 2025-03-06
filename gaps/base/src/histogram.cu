e7628d960a932c31cf7f24f441f1033fb2b06868
siddharthsule <siddharthsule@outlook.com>
2025-03-06 20:38:57 +0000

    correct yoda output

--- gaps/base/src/histogram.cu
+++ gaps/base/src/histogram.cu
@@ -17,12 +17,13 @@ std::string to_string(histo1d h, std::string name) {
   ss << "Title=\nType=Histo1D\n";
   ss << "# ID\tID\tsumw\tsumw2\tsumwx\tsumwx2\tnumEntries\n";
   ss << std::scientific << std::setprecision(6);
-  ss << "Total" << "\t" << h.total.w << "\t" << h.total.w2 << "\t" << h.total.wx
-     << "\t" << h.total.wx2 << "\t" << static_cast<int>(h.total.n) << "\n";
-  ss << "Underflow" << "\t" << h.uflow.w << "\t" << h.uflow.w2 << "\t"
-     << h.uflow.wx << "\t" << h.uflow.wx2 << "\t" << static_cast<int>(h.uflow.n)
+  ss << "Total\tTotal" << "\t" << h.total.w << "\t" << h.total.w2 << "\t"
+     << h.total.wx << "\t" << h.total.wx2 << "\t" << static_cast<int>(h.total.n)
      << "\n";
-  ss << "Overflow" << "\t" << h.oflow.w << "\t" << h.oflow.w2 << "\t"
+  ss << "Underflow\tUnderflow" << "\t" << h.uflow.w << "\t" << h.uflow.w2
+     << "\t" << h.uflow.wx << "\t" << h.uflow.wx2 << "\t"
+     << static_cast<int>(h.uflow.n) << "\n";
+  ss << "Overflow\tOverflow" << "\t" << h.oflow.w << "\t" << h.oflow.w2 << "\t"
      << h.oflow.wx << "\t" << h.oflow.wx2 << "\t" << static_cast<int>(h.oflow.n)
      << "\n";
   ss << "# xlow\txhigh\tsumw\tsumw2\tsumwx\tsumwx2\tnumEntries\n";
